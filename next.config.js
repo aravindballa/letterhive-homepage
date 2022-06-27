91b66012913fb22d18cae5b624b79dd14b95272f
Aravind Balla <bsaaravind@gmail.com>
2022-06-27 16:06:19 +0530

    fix: use cloudinary loader

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    loader: 'cloudinary',
+    path: 'https://images.balla.dev',
+  },
 }
 
 module.exports = nextConfig
