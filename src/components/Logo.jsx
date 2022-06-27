b23e8c765524eebaae95a862b468beb6b5b05347
Aravind Balla <bsaaravind@gmail.com>
2022-06-27 18:25:08 +0530

    colors

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,6 +1,6 @@
 export function Logo(props) {
   return (
-    <div className="flex gap-x-2 font-display text-xl font-bold">
+    <div className="flex gap-x-2 font-display text-xl font-bold text-slate-900">
       <span role="img">🍯</span>
       <span>Letterhive</span>
     </div>
