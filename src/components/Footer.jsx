1f2dd07ebbed27e444c396d9683e5e1353f9eed0
Aravind Balla <bsaaravind@gmail.com>
2022-08-20 20:03:19 +0530

    fix: twitter link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -37,7 +37,7 @@ export function Footer() {
         </div>
         <div className="flex flex-col items-center border-t border-slate-400/10 py-10 sm:flex-row-reverse sm:justify-between">
           <div className="flex space-x-6">
-            <Link href="https://twitter.com/letterhive_a[[">
+            <Link href="https://twitter.com/letterhive_app">
               <a className="group">
                 <span className="sr-only">Letterhive on Twitter</span>
                 <svg
