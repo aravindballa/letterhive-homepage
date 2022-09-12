bca3536d77c1b1dff0f0a23bd102faa878ca8bfa
Aravind Balla <bsaaravind@gmail.com>
2022-09-12 17:22:04 +0530

    fix: keep only valid data

--- src/data/showcase.js
+++ src/data/showcase.js
@@ -11,12 +11,6 @@ const data = [
     url: 'https://cognition.letterhive.com',
     image_src: '/showcase/cognition.png',
   },
-  {
-    name: 'Blockchain',
-    author: 'Rittik Basu',
-    url: 'https://blockchain.letterhive.com',
-    image_src: '/showcase/cognition.png',
-  },
 ]
 
 export default data
