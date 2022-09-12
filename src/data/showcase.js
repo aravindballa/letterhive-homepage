9493f27c0ce00bdd94fac80fa2bc4f3f9d53c976
rittikbasu <irittik@gmail.com>
2022-09-12 16:23:13 +0530

    fix: addblog name + fixed formatting

---
+++ src/data/showcase.js
@@ -0,0 +1,22 @@
+const data = [
+  {
+    name: 'Hackletter',
+    author: 'Aravind Balla',
+    url: 'https://hackletter.letterhive.com',
+    image_src: '/showcase/hackletter.png',
+  },
+  {
+    name: 'Cognition',
+    author: 'Vikra Vardhan',
+    url: 'https://cognition.letterhive.com',
+    image_src: '/showcase/cognition.png',
+  },
+  {
+    name: 'Blockchain',
+    author: 'Rittik Basu',
+    url: 'https://blockchain.letterhive.com',
+    image_src: '/showcase/cognition.png',
+  },
+]
+
+export default data
